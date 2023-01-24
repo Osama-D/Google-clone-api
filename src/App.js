8b96982ea20fa2ff68da06c217b4610d71c730bc
Osama-D <musashioussama457@gmail.com>
2023-01-24 21:26:44 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 import Nav from "./components/Nav.jsx";
-
 import Router from "./components/Router.jsx";
-
-import { useLocation } from "react-router-dom";
 
 function App() {
   const [dartTheme, setDarkTheme] = useState(false);
-  const location = useLocation();
   return (
     <div className={dartTheme ? "dark" : ""}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen overflow-x-hidden">
